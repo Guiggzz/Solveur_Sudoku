19d3dd68af9205e6ff431f172c619ecd66a04367
guigz <guillaume.breon@gmail.com>
2023-12-20 15:24:46 +0100

    loadfromfile et __consruct

--- src/SudokuGrid.php
+++ src/SudokuGrid.php
@@ -2,5 +2,25 @@
 
 class SudokuGrid implements GridInterface
 {
-    /* Insérer le code ici */
+    public array $data;
+    
+    public static function loadFromFile($filepath): ?SudokuGrid{
+        
+        $contenu_fichier = file_get_contents($filepath);
+        $data = json_decode($contenu_fichier, true);
+        if ($data === null) {
+           echo "Erreur lors du décodage du fichier JSON.";
+           return null;
+        } 
+        else {
+            return new SudokuGrid($data);
+        }
+
+    }
+
+    public function __construct(array $data){
+        $this->data = $data;
+    }
+
+
 }
