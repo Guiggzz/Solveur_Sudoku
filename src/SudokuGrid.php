76da91f567385b8c562465aa739b23b75440d8a7
guigz <guillaume.breon@gmail.com>
2023-12-20 17:38:43 +0100

    square implémentation

--- src/SudokuGrid.php
+++ src/SudokuGrid.php
@@ -40,20 +40,41 @@ class SudokuGrid implements GridInterface
     //! Methode row qui permet de lire les données d'une ligne
 
     public function row(int $rowIndex): array{
-        foreach($this->data[$rowIndex] as $Values ){
-            $row_val_r[] = $Values;
-        }
-        return $row_val_r;
+        return $this->data[$rowIndex];
     }
 
     //! Methode column qui permet de lire les données d'une colonne
 
     public function column(int $columnIndex): array{
-        foreach($this->data[$columnIndex] as $Values ){
-            $row_val_r[] = $Values;
-        }
-        return $row_val_r;
+        return array_column($this->data, $columnIndex);
     }
+
+    //! Methode square qui permet de lire les données d'un carré à partir d'un index donné
+
+    public function square(int $squareIndex): array{
+        $tailleBloc = 3;
+        $ligneDebut = floor($squareIndex / $tailleBloc) * $tailleBloc;
+        $colonneDebut = ($squareIndex % $tailleBloc) * $tailleBloc;
+
+        $bloc = [];
+
+        for ($i = $ligneDebut; $i < $ligneDebut + $tailleBloc; $i++) {
+            for ($j = $colonneDebut; $j < $colonneDebut + $tailleBloc; $j++) {
+                $bloc[] = $this->data[$i][$j];
+            }
+        }
+
+        return $bloc;
+        }
+
+    //! Mehode de parcours de la grille
+    
+    public function display(): string{
+        return json_encode($this->data);
+
+    }
+
+
 
 
 
